1a131f18f0e7f2b7fc953194d6b17e47a6cdaf47
psdrski <sadurski.p001@gmail.com>
2026-04-09 08:50:17 +0200

    wip: fastapi setup

--- config/fastapi/app/main.py
+++ config/fastapi/app/main.py
@@ -1,3 +1,6 @@
 from fastapi import FastAPI
+from app.routers.static_endpoint import router
 
 app = FastAPI()
+
+app.include_router(router, prefix="/app")
---
+++ config/fastapi/app/routers/static_endpoint.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter
+
+router = APIRouter()
+
+@router.get("/user")
+async def get_user():
+    return [{"id":123132131321, "name":"Mikołaj", "location":"Kielce", "posts": 12},
+            {"id": 123132131321, "name": "Piotr", "location": "Garwolin", "posts": 32},
+            {"id": 123132131321, "name": "Rafal", "location": "Turek", "posts": 42},
+            {"id": 123132131321, "name": "Adam", "location": "Szczecin", "posts": 22},
+            {"id": 123132131321, "name": "Ola", "location": "Bialystok", "posts": 11},]
+
